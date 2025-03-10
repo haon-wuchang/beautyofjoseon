faba4fe0e6a3e4a449af44ea6e146e2d406a0bae
haon <haoony@naver.com>
2025-03-10 14:13:09 +0900

    3/10

--- client/src/page/Layout.jsx
+++ client/src/page/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
+import Header from '../component/Header.jsx';
+import Footer from '../component/Footer.jsx';
 
 export default function Layout() {
     return (
