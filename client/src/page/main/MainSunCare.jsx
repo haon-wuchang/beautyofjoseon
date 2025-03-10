1e74a130c126df591e98a8ee7452d6982037affe
YuNa <dbsk4030@gmail.com>
2025-03-10 18:05:50 +0900

    25. 03. 10 yuna commit

---
+++ client/src/page/main/MainSunCare.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import MainProductBlock from '../../component/MainProductBlock';
+import MainTitleBlock from '../../component/MainTitleBlock';
+
+export default function MainSunCare() {
+    return (
+        <div className='main-contents-suncare-wrap'>
+            <div className='main-contents-suncare-top'>
+                <img className='main-contents-suncare-top-img' src="images/main_suncare.jpg" alt="sunblock_img" />
+                <div className='main-contents-suncare-info'>
+                    <p>Suncare Essentials</p>
+                    <p>올바른 스킨케어의 마무리, 선케어</p>
+                    <p>
+                        <p>피부 본연의 아름다움을 중시한 조선시대의 단장은 화려한 꾸밈이 아닌,</p>
+                        <p>단정하고 깨끗하게 가꾸기를 게을리하지 않는 것이었습니다.</p>
+                        <p>조선시대 미의 철학을 새기며, 매일 실천해야 할 선케어 3종을 소개합니다.</p>
+                        <p>당신의 피부에 꼭 맞는 한방 선케어를 찾아보세요.</p>
+                    </p>
+                </div>
+            </div>
+            <div>
+                <MainTitleBlock 
+                    divClassName="suncare-title"
+                    title="Suncare Product"
+                />
+                <MainProductBlock className="main-contents-suncare" />
+            </div>
+        </div>
+    );
+}
---
+++ client/src/page/main/MainTopSlide.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainTopSlide() {
+    return (
+        <div className='main-top-slide-wrap'>
+            <img style={{width: "100%"}} src="images/ad1.jpeg" alt="" />
+        </div>
+    );
+}
