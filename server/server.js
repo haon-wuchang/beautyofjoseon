f0e9977c35765ff18a17ce3e2579ad45d4281825
YuNa <dbsk4030@gmail.com>
2025-03-13 18:25:29 +0900

    250312 yuna

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-<<<<<<< HEAD
-import productRouter from './router/productRouter.js';
-import mainRouter from './router/mainRouter.js';
-=======
 import productRouter from './router/productRouter.js'
 import LoginRouter from './router/LoginRouter.js';
 import productRouter from './router/productRouter.js';
 import mypageRouter from './router/mypageRouter.js';
->>>>>>> 8cd6f19619b7abe5c3986224ac8e0d4f70c56ab4
 
 
 
@@ -24,18 +19,11 @@ server.use(cors());
 // server.use('/upload_files', express.static(path.join("upload_files")));
 
 
-<<<<<<< HEAD
-
-// 서버의 요청처리를 위한 미들웨어 정의 //
-server.use('/product', productRouter);
-server.use('/main', mainRouter);
-=======
 /* 서버의 요청처리를 위한 미들웨어 정의 */
 server.use('/product', productRouter)
 server.use('/product', productRouter);
 server.use('/login',LoginRouter);
 server.use('/mypage',mypageRouter);
->>>>>>> 8cd6f19619b7abe5c3986224ac8e0d4f70c56ab4
 
 
 
