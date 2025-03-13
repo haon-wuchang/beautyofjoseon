47c5ffa9b93dcd72f4995832b2d0feab57fab23e
YuNa <dbsk4030@gmail.com>
2025-03-13 18:10:17 +0900

    25. 03. 12 yuna-main,header

--- server/repository/productRepository.js
+++ server/repository/productRepository.js
@@ -1,3 +1,5 @@
+import { db } from './db.js';
+
 export const getProduct = async(pid) => {
 
     const sql = `
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import productRouter from './router/productRouter.js';
+import mainRouter from './router/mainRouter.js';
 
 const server = express();
 const port = 9000;
@@ -13,8 +16,8 @@ server.use('/upload_files', express.static(path.join("upload_files")));
 
 
 // 서버의 요청처리를 위한 미들웨어 정의 //
-server.use('/product', productRouter)
-
+server.use('/product', productRouter);
+server.use('/main', mainRouter);
 
 
 
