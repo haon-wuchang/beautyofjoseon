b1233b6a2327ba8537d251e1baf4a465fff52726
YuNa <dbsk4030@gmail.com>
2025-03-14 18:10:13 +0900

    Header 수정

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../auth/AuthContext.js';
 import { Link, useNavigate } from 'react-router-dom';
 import { GoPerson } from "react-icons/go";
