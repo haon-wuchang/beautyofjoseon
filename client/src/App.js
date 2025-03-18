83476a8ee74f8f1f90d3be3abaa4aba17406d076
dahee <kimdahee42@gmail.com>
2025-03-18 18:23:02 +0900

    1차 회원가입 커밋밋

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './style/common.css';
 import './style/haon.css';
 import './style/yuna.css';
+import './style/dahee.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './page/Layout.jsx';
 import Home from './page/Home.jsx';
